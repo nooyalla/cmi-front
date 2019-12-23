6d216556062d235fb88fde5ff9950f397b450674
yohai zvuloon <yohai.zvuloon@autodesk.com>
2019-12-23 14:12:20 +0200

    clear css files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-import './style/bootstrap.css';
-import './style/loader.css';
-import './style/index.css';
-import './style/login.css';
-import './style/create-event-form.css';
-console.log('before all')
 ReactDOM.render(
   <App />,
   document.getElementById('root')
